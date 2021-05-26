50d98b758cbb110638aa34150be700311578749b
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-05-26 17:45:32 -0300

    M1B5D1 - changes to script.js

--- Modulos/1 - Fundamentos do Desenvolvimento Web/5 - JavaScript: DOM, Eventos e Web Storage/1 - DOM e seletores/script.js
+++ Modulos/1 - Fundamentos do Desenvolvimento Web/5 - JavaScript: DOM, Eventos e Web Storage/1 - DOM e seletores/script.js
@@ -1,17 +1,20 @@
 document.getElementById('header-container').style.backgroundColor = 'green';
 
-const purple = document.querySelectorAll('.emergency-tasks div h3');
-for (i of purple) {
+// const purple = document.querySelectorAll('.emergency-tasks div h3');
+// for (i of purple) {
+//   i.style.backgroundColor = 'purple';
+// }
+
+for (i of document.querySelectorAll('.emergency-tasks div h3')) {
   i.style.backgroundColor = 'purple';
 }
 
-const pink = document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
 
-const black = document.querySelectorAll('.no-emergency-tasks div h3');
-for (i of black) {
+for (i of document.querySelectorAll('.no-emergency-tasks div h3')) {
   i.style.backgroundColor = 'black';
 }
 
-const yellow = document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
 
 document.getElementById('footer-container').style.backgroundColor = '#2c4d37'
