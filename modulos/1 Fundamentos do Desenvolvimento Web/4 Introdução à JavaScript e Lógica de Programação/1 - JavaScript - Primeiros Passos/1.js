1f507120b3c3b1e19218952ab52e51cbf765638b
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-07-13 14:32:01 -0300

    rename folders

--- modulos/1 Fundamentos do Desenvolvimento Web/4 Introdução à JavaScript e Lógica de Programação/1 - JavaScript - Primeiros Passos/1.js
+++
@@ -1,8 +0,0 @@
-let a = 7;
-let b = 5;
-
-console.log('Soma: ' + (a+b));
-console.log('Subtração: ' + (a-b));
-console.log('Multiplicação: ' + (a*b));
-console.log('Divisão: ' + (a/b));
-console.log('Resto da divisão: ' + (a%b));
